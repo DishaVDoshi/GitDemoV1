1b70a62b04670e5ad7ddabbfde22c9a3f5ecdd62
Disha <dishavdoshi@gmail.com>
2022-02-04 14:28:05 +1100

    Commit message

---
+++ src/Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		
+			String school="y";
+			if(school=="y")
+			{
+				System.out.println("Lunch box");
+			}
+			else
+			{
+				System.out.println("no need");
+			}
+		
+	}
+
+}
